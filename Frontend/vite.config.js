9b5063f642aa6edd3fdf5fdc7913bed15b232f56
HarshAvichal <avichal.harsh14@gmail.com>
2024-12-21 00:41:41 -0500

    commit changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react", // React core module
-        "react-dom", // React DOM
-        "react-toastify", // For toast notifications
-        "react-lottie", // For lottie animations
-        "swiper", // For swiper carousel
-        "@fortawesome/react-fontawesome", // FontAwesome icons
-        "@fortawesome/free-solid-svg-icons", // Solid icons from FontAwesome
-        "react-typed", // Typed animations
-      ],
-    },
-  },
-});
+})
