072b03a1c9553e002fb7d6fe1518f22975468316
HarshAvichal <avichal.harsh14@gmail.com>
2024-12-21 00:46:01 -0500

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [], // Ensure no critical external modules are left out
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
